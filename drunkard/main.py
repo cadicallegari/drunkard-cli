84dade02b5c88742aeca5cb72e2c2a8a096c9f92
Claudinei Callegari <cadicallegari@gmail.com>
2018-09-04 22:03:33 -0300

    mplement basic fail recover

--- drunkard/main.py
+++ drunkard/main.py
@@ -1,6 +1,7 @@
 import json
 import pathlib
 import logging
+from os import path
 
 import click
 import click_log
@@ -9,24 +10,65 @@ import requests
 logger = logging.getLogger(__name__)
 click_log.basic_config(logger)
 
+_INDEX_FILE_NAME = ".drunkard.index"
 
-def data_sender(loader, url):
+
+def _save_index_file(index_info, posix_file, index, success):
+    index_info[posix_file.name] = {"index": index, "success": success}
+    with open(path.join(posix_file.parent, _INDEX_FILE_NAME), 'w') as f:
+        f.write(json.dumps(index_info))
+
+
+def _load_index_file(posix_file):
+        filepath = path.join(posix_file.parent, _INDEX_FILE_NAME)
+        if not path.isfile(filepath):
+            return {}
+
+        with open(filepath, "r") as f:
+            content = f.read()
+            if len(content) > 2:
+                return json.loads(content)
+            return {}
+
+
+def data_sender(files, url):
     count = 0
-    for r in loader():
-        try:
-            r = requests.post(url, json=r)
-            logger.debug(r.status_code)
-            if r.status_code < 300:
-                count += 1
 
-        except requests.exceptions.RequestException as e:
-            logger.error(e)
-            return
-        except Exception as e:
-            logging.exception(e)
-            return
+    for f in files():
+        dir_info = _load_index_file(f)
+        if f.name in dir_info and dir_info[f.name].get("success", False):
+            continue
+
+        current_index = 0
+        sent_index = dir_info.get(f.name, {"index": 0}).get("index", 0)
+
+        for r in load_json(f):
+            if sent_index > current_index:
+                current_index += 1
+                continue
+
+            try:
+                r = requests.post(url, json=r)
+                logger.debug(r.status_code)
+                if r.status_code < 300:
+                    count += 1
+
+            except requests.exceptions.RequestException as e:
+                logger.error(e)
+                _save_index_file(dir_info, f, current_index, False)
+                return count
+
+            current_index += 1
+
+        _save_index_file(dir_info, f, current_index, True)
 
     return count
+
+
+def load_json(filename):
+    with open(filename) as f:
+        for line in f:
+            yield json.loads(line)
 
 
 def find_files(directory, extensions):
@@ -37,19 +79,6 @@ def find_files(directory, extensions):
             for fn in pathlib.Path(directory).glob(pattern):
                 yield fn
     return files
-
-
-def load_json(files_finder):
-    def load():
-        for fn in files_finder():
-            with open(fn) as f:
-                for line in f:
-                    yield json.loads(line)
-    return load
-
-
-def data_loader(directory, extensions):
-    return load_json(find_files(directory, extensions))
 
 
 @click.command()
@@ -70,11 +99,17 @@ def data_loader(directory, extensions):
     default='json,jsonl',
     help='file extensions'
 )
+@click.option(
+    '--force', '-f',
+    type=click.BOOL,
+    default=False,
+    help='force send all data again'
+)
 @click_log.simple_verbosity_option(logger)
-def cli(url, directory, extensions):
-    loader = data_loader(directory, extensions)
+def cli(url, directory, extensions, force):
+    file_loader = find_files(directory, extensions)
     logger.info("Sending ...")
-    count = data_sender(loader, url)
+    count = data_sender(file_loader, url)
     logger.info(f"Done: {count} records sent")
 
 
